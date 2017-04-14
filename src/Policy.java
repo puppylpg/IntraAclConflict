61335fa181bc3a3be603a17e674a7e1ee81e78f7
puppylpg <shininglhb@163.com>
2017-04-14 16:39:26 +0800

    check list and delete useless one

--- src/Policy.java
+++ src/Policy.java
@@ -63,6 +63,30 @@ public class Policy {
         return ip;
     }
 
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
     @Override
     public String toString() {
         return "Policy{" +
--- src/PolicyCheck.java
+++ src/PolicyCheck.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by bishe2016 on 下午2:18 17-4-14.
  */
 public class PolicyCheck {
 
+    /**
+     * compare IP
+     * @param ipA
+     * @param ipB
+     * @return = or > or < or !=
+     */
     static String compareIP(String ipA, String ipB) {
         if(ipA.equals(ipB)) {
             return Constant.COMP_EQUAL;
@@ -25,6 +32,12 @@ public class PolicyCheck {
         return Constant.COMP_EQUAL;                 // useless
     }
 
+    /**
+     * the result of comparision in srcIP & desIP
+     * @param srcIPComp
+     * @param desIPComp
+     * @return >= or <= or !=
+     */
     static String ipComps(String srcIPComp, String desIPComp) {
         if((srcIPComp.equals(Constant.COMP_BIGGER) && desIPComp.equals(Constant.COMP_BIGGER))
                 || (srcIPComp.equals(Constant.COMP_EQUAL) && desIPComp.equals(Constant.COMP_EQUAL))
@@ -42,15 +55,77 @@ public class PolicyCheck {
         return Constant.COMP_NOTEQUAL;
     }
 
+    /**
+     * check list. if policies need to be deleted, mark in valid[] and deal it at last.
+     * @param policies
+     */
     static void checkList(ArrayList<Policy> policies) {
-//        Iterator<Policy> it = policies.iterator();
-//        while (it.hasNext()) {
+        // validity to mark
+        int len = policies.size();
+        boolean [] valid = new boolean[len];
+        for(int i=0; i < len; i++) {
+            valid[i] = true;
+        }
+
+        for (int i = 0; i < policies.size(); ++i) {
+            if(!valid[i]) {                                         // check validity
+                continue;
+            }
+            Policy curPolicy = policies.get(i);
+            for(int j = 0; j < i; ++j) {
+                if(!valid[j]) {                                     // check validity
+                    continue;
+                }
+                Policy prePolicy = policies.get(j);
+                String preSrcIP = prePolicy.getSrc();
+                String curSrcIP = curPolicy.getSrc();
+                String preDesIP = prePolicy.getDes();
+                String curDesIP = curPolicy.getDes();
+                String preAction = prePolicy.getAction();
+                String curAction = curPolicy.getAction();
+                String srcIPComp = compareIP(preSrcIP, curSrcIP);
+                String desIPComp = compareIP(preDesIP, curDesIP);
+
+                String ipComp = ipComps(srcIPComp, desIPComp);      //result of comparision in srcIP & desIP
+
+                if(ipComp.equals(Constant.COMP_BE)) {               // Shadow
+                    System.out.println("SHADOW policy: " + curPolicy.toString() + " by " +
+                            prePolicy.toString());
+                    System.out.println("Delete latter policy: " + curPolicy.toString());
+                    valid[i] = false;
+                    break;
+                } else if(ipComp.equals(Constant.COMP_LE)) {
+                    if(preAction.equals(curAction)) {               // Redundant
+                        System.out.println("REDUNDANT policy: " + prePolicy.toString() + " with "
+                                + curPolicy.toString());
+                        System.out.println("Delete former policy: " + prePolicy.toString());
+                        valid[j] = false;
+                    } else {                                        // Special case
+                        System.out.println("SPECIAL_CASE policy: " + prePolicy.toString() + " of "
+                                + curPolicy.toString());
+                    }
+                }
+            }
+        }
+
+        // delete at last according to the validity
+        Iterator<Policy> it = policies.iterator();
+        int i = 0;
+        while (it.hasNext()) {
+            it.next();
+            if(!valid[i++]) {
+                it.remove();
+            }
+        }
+    }
+
+    /**
+     * check list. if policies need to be deleted, recheck.
+     * @param policies
+     */
+    static void checkListCycle(ArrayList<Policy> policies) {
         for (int i = 0; i < policies.size(); ++i) {
             Policy curPolicy = policies.get(i);
-//            Policy curPolicy = it.next();
-//            Iterator<Policy> iterator = policies.iterator();
-//            Policy prePolicy = iterator.next();
-//            while (!curPolicy.toString().equals(prePolicy.toString())) {
             for(int j = 0; j < i; ++j) {
                 Policy prePolicy = policies.get(j);
                 String preSrcIP = prePolicy.getContextByField(Constant.FIELD_SOURCE);
@@ -63,35 +138,29 @@ public class PolicyCheck {
                 String desIPComp = compareIP(preDesIP, curDesIP);
 
                 String ipComp = ipComps(srcIPComp, desIPComp);
-                //Shadow
-                if(ipComp.equals(Constant.COMP_BE)) {
+
+                if(ipComp.equals(Constant.COMP_BE)) {               // Shadow
                     System.out.println("SHADOW policy: " + curPolicy.toString() + " by " +
                                         prePolicy.toString());
                     System.out.println("Delete latter policy: " + curPolicy.toString());
                     policies.remove(i);
-                    checkList(policies);
+                    checkListCycle(policies);
                     return;
-//                    it.remove();
-//                    break;
                 } else if(ipComp.equals(Constant.COMP_LE)) {
-                    //Redundant
-                    if(preAction.equals(curAction)) {
+                    if(preAction.equals(curAction)) {               // Redundant
                         System.out.println("REDUNDANT policy: " + prePolicy.toString() + " with "
                                             + curPolicy.toString());
                         System.out.println("Delete former policy: " + prePolicy.toString());
                         policies.remove(j);
-                        checkList(policies);
+                        checkListCycle(policies);
                         return;
-//                        iterator.remove();
-                    } else {
+                    } else {                                        // Special case
                         System.out.println("SPECIAL_CASE policy: " + prePolicy.toString() + " of "
                                             + curPolicy.toString());
                     }
                 }
-//                prePolicy = iterator.next();
             }
         }
-
     }
 
     public static void main(String [] args) {
